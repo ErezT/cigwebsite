dea0724cdbacab2f8cbea13761240f2bc1888583
ErezT <erezpitke@gmail.com>
2016-11-24 23:54:40 +0200

    Added constants

--- javascript.js
+++ javascript.js
@@ -5,16 +5,18 @@ var total_cigs = 0;
 var price_per_pack = 0;
 var final_amount = 0;
 var packs_per_month = 0;
-var cigs_per_pack = 50;
+const CIGS_PER_PACK = 50;
+const AVG_DAYS_PER_MONTH = 30;
+const DAYS_IN_YEAR = 365;
 
 function numberOfCigarettes(){
     cigs_per_day = document.getElementById("cigs_per_day").value;
     years = document.getElementById("years").value;
     packs_per_month = document.getElementById("packs_per_month").value;
     if (packs_per_month > 0 ) {
-        total_cigs = (packs_per_month * cigs_per_pack) / 30 * 365 * years;
+        total_cigs = (packs_per_month * CIGS_PER_PACK) / AVG_DAYS_PER_MONTH * DAYS_IN_YEAR * years;
     } else {
-        total_cigs = cigs_per_day * 365 * years;
+        total_cigs = cigs_per_day * DAYS_IN_YEAR * years;
     }
     var brand = document.getElementById("cig_selector").value;
     document.getElementById("num_smoked_result").innerHTML =  total_cigs + " " + brand + " cigarettes";
